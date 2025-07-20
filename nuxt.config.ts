55b000d06cad672ab4642c8a57008c2bc20059c4
Dan <danielyarza1@gmail.com>
2025-07-20 12:10:54 +0200

    adsense and analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/supabase', '@nuxtjs/tailwindcss',
-  ],
+  modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', 'nuxt-gtag', '@nuxtjs/google-adsense'],
+
+  gtag: {
+    id: 'G-248JBX6J6P',
+  },
+  googleAdsense: {
+    id: 'ca-pub-1741608972034726'
+  },
+
   supabase: {
     redirect: false,
   },
